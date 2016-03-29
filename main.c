cf5897968d981d6cfa0623936344bd2620cc1a33
Mihail <fsvymk@gmail.com>
2016-03-29 21:39:56 +0300

    VOLT:DC:RANG?

--- main.c
+++ main.c
@@ -4,20 +4,29 @@
 
 int main() {
     char byte;
-    char *buf[1024];
+    char *buf[32];
+
+    int i;
+    for(i=0;i<32;i++) {
+        buf[i] = 0;
+    }
 
     int fd = open("/dev/usbtmc0", O_RDWR);
 
     //char *command  = "VOLT:DC:RANG 7\n";
-    char *command  = "MEAS:DIOD?\n";
-    char *command2 = "VOLT:DC:RANG?\n";
+    //char *command  = "MEAS:DIOD?\n";
+    char *command = "VOLT:DC:RANG?\n";
     int so = sizeof(command);
 
-    write(fd, command,  11);
+    write(fd, command,  13);
     //write(fd, command2, 18);
 
-    ssize_t size = read(fd, &buf, 512);
-    printf("Read byte %c\n", byte);
+    ssize_t size = read(fd, &buf, 32);
+
+    char x = buf[0];
+    char y = buf[1];
+
+    printf("Read byte %d %d | %c %c\n", x, y , buf[0], buf[1]);
     printf("Size %d\n", size);
 
     return 0;
